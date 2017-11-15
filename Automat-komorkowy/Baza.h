22047500102c28af58368bfbd414b898e1e8b802
Jakub Kopka <kuba@surfportal.pl>
2017-11-15 21:48:43 +0100

    Zapis

---
+++ Automat-komorkowy/Baza.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+#include <list>
+#include <list>
+using namespace std;
+class Baza
+{
+public:
+	void zapisz(string nazwa_pliku, Pole pole);
+	void wczytaj(string nazwa_pliku);
+
+};
