c7992a6dfc11679c401f3d066974ef3af9edc44d
Shaman <matejukmaciej@gmail.com>
2018-01-10 18:06:46 +0100

    Finished commenting

--- Automat-komorkowy/Baza.h
+++ Automat-komorkowy/Baza.h
@@ -12,7 +12,9 @@ void operator<<(std::ostream& os, vector<vector<Komorka>>& v);
 class  Baza
 {
 public:
+	//! Save funtion creating file and filling if based on Pole provided in argument
 	void static zapisz(string nazwa_pliku, Pole pole);
+	//! Load function creating new field based on data provided in file under nazwa_pliku.
 	void static wczytaj(string nazwa_pliku, Pole *& pole);
 
 };
